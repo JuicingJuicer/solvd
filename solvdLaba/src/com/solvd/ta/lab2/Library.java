9902326450f37f897bb6a6dfd1f182fbce564fc6
Vincent <56519669+JuicingJuicer@users.noreply.github.com>
2023-04-15 03:09:28 -0700

    Added library class

---
+++ solvdLaba/src/com/solvd/ta/lab2/Library.java
@@ -0,0 +1,32 @@
+package com.solvd.ta.lab2;
+
+public class Library {
+	private String name;
+	private String city;
+
+	public Library(String name, String city) {
+		this.name = name;
+		this.city = city;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getCity() {
+		return this.city;
+	}
+
+	public static void main(String[] args) {
+		Library library = new Library("Juice Library", "Stockton");
+		System.out.println("Hi welcome to " + library.getName() + " of " + library.getCity() + "!");
+	}
+}
