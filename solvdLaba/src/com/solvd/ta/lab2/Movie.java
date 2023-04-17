9b2bcc22de80d377844ddb1fa7f8e44b4000374c
Vincent <56519669+JuicingJuicer@users.noreply.github.com>
2023-04-17 03:44:29 -0700

    Added movie class

---
+++ solvdLaba/src/com/solvd/ta/lab2/Movie.java
@@ -0,0 +1,41 @@
+package com.solvd.ta.lab2;
+
+import java.util.EnumSet;
+
+public class Movie extends Media {
+	private String director;
+	private int runtime;
+	private double rating;
+
+	public Movie(String title, int year, EnumSet<Genre> genre, String director, int runtime, double rating) {
+		super(title, year, genre);
+		this.director = director;
+		this.runtime = runtime;
+		this.rating = rating;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public void setRuntime(int runtime) {
+		this.runtime = runtime;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public String getDirecttor() {
+		return this.director;
+	}
+
+	public int getRuntime() {
+		return this.runtime;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+}
