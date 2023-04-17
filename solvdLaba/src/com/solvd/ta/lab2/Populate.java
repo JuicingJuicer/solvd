f06412ae14b0765889264ac2c666df4ca521fa05
Vincent <56519669+JuicingJuicer@users.noreply.github.com>
2023-04-17 04:38:48 -0700

    add populate class

---
+++ solvdLaba/src/com/solvd/ta/lab2/Populate.java
@@ -0,0 +1,26 @@
+package com.solvd.ta.lab2;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+
+public class Populate {
+	ArrayList<Media> arr = new ArrayList<Media>();
+
+	public void generateBooks() {
+		Book dune = new Book("Dune", 1965, EnumSet.of(Genre.SCIFI), "Frank Herbert", 896);
+		addBooks(dune);
+		if (arr.get(0) instanceof Book) {
+			Book book = (Book) arr.get(0);
+			book.printDetails();
+		}
+	}
+
+	public void addBooks(Media m) {
+		arr.add(m);
+	}
+
+	public static void main(String[] args) {
+		Populate a = new Populate();
+		a.generateBooks();
+	}
+}
