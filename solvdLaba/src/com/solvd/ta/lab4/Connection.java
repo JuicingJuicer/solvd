533e596d62f0e8b77b112151887312d09b3c55ac
Vincent <56519669+JuicingJuicer@users.noreply.github.com>
2023-05-20 05:49:55 -0700

    minor fixes

--- solvdLaba/src/com/solvd/ta/lab4/Connection.java
+++ solvdLaba/src/com/solvd/ta/lab4/Connection.java
@@ -1,8 +1,13 @@
 package com.solvd.ta.lab4;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Connection {
 
+	final Logger LOGGER = LogManager.getLogger(Connection.class);
+
 	public void connect() {
-		System.out.println("Connected!");
+		LOGGER.info("Connected!");
 	}
 }
--- solvdLaba/src/com/solvd/ta/lab4/Multi.java
+++ solvdLaba/src/com/solvd/ta/lab4/Multi.java
@@ -1,25 +1,25 @@
 package com.solvd.ta.lab4;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Multi {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		final Logger LOGGER = LogManager.getLogger(Multi.class);
 		ConnectionPool pool = new ConnectionPool(5);
-		ConnectionPool pool2 = new ConnectionPool(5);
 		ExecutorService ex = Executors.newFixedThreadPool(7);
-		ExecutorService ex2 = Executors.newFixedThreadPool(7);
 		Runnable task = new Runnable() {
 			@Override
 			public void run() {
 				try {
 					Connection c = pool.getConnection();
 					c.connect();
-					Thread.sleep(1555);
-					System.out.println("Job done!");
+					Thread.sleep(2000);
+					LOGGER.info("Job done!");
 					pool.releaseConnection(c);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
@@ -31,27 +31,6 @@ public class Multi {
 			ex.execute(task);
 		}
 
-		Callable<String> callable = new Callable<String>() {
-			@Override
-			public String call() {
-				try {
-					Connection c = pool2.getConnection();
-					c.connect();
-					Thread.sleep(1000);
-					pool2.releaseConnection(c);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				return "Job done!";
-			}
-		};
-
-		for (int i = 0; i < 7; i++) {
-			Future<String> future = ex2.submit(callable);
-			while (!future.isDone()) {
-				System.out.println("Not done!");
-				Thread.sleep(500);
-			}
-		}
+		ex.shutdown();
 	}
 }
